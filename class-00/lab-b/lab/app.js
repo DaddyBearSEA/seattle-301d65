c7d5f24219b2ee191e0e8c913f61279b27bffeb2
DaddyBearSEA <michael@massagepractice101.com>
2020-08-08 18:59:30 -0700

    completed arrow functions

--- class-00/lab-b/lab/app.js
+++ class-00/lab-b/lab/app.js
@@ -108,42 +108,60 @@ console.log('Hello from the new object function', newObject(['hi', 'hello', 'are
 
 
 
-let sum = function(a, b, c, d) {
-  return a + b + c + d;
-};
+let sum = (a, b, c, d) => `${a + b + c + d}`;
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(sum(1, 2, 3, 4));
+console.log(sum(1, 2, 3, 4));
 
 
-let objectLit = function() {
-  return {
+// let objectLit = function() {
+//   return {
+//     key1: 'value1',
+//     key2: 'value2',
+//     key3: 'value3',
+//   };
+// };
+
+let objectLit = array => ({
     key1: 'value1',
     key2: 'value2',
     key3: 'value3',
-  };
-};
+});
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(objectLit());
+console.log(objectLit());
 
 
-let sumAndProduct = function(a, b) {
-  let sum = a + b;
-  let product = a * b;
-  return [sum, product];
-};
+// let sumAndProduct = function(a, b) {
+//   let sum = a + b;
+//   let product = a * b;
+//   return [sum, product];
+// };
+
+
+let sumAndProduct = (a, b) => `${a + b} ${a * b}`;  // TODO: can't get the '2' to show up??  .map?
+//   sum = (a + b);
+//   let product = a * b;
+//   return [sum, product];
+// });
+
+
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(sumAndProduct(3, 9));
+console.log(sumAndProduct(3, 9));
 
 
-let message = function(name) {
-  return `Hello, ${name}!`;
-};
+// let message = function(name) {
+//   return `Hello, ${name}!`;
+// };
+
+
+let message = name => (`Hello, ${name}`);
+
+
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(message('Allie'));
+console.log(message('Allie'));
 
 
 let Student = function(name, age, hometown) {
@@ -152,11 +170,23 @@ let Student = function(name, age, hometown) {
   this.hometown = hometown;
 };
 
+
+// let Student = (name, age, hometown) => `${name}, ${age}, ${hometown}`
+
+
+
 let joe = new Student('Joe', 'Schmoe', 100);
+// let joe = ('Joe', 'Schmoe', 100);
+
+
+
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this code to break!
-// console.log(joe);
+console.log(joe);
+
+
+
 
 // TODO: After viewing the previous console.log(), return the code to a working state.
 
@@ -166,20 +196,30 @@ Student.prototype.greeting = function() {
   return `Hi, my name is ${this.name}`;
 };
 
+// Student.prototype.greeting = () => `Hi, my name is ${name}`
+
+
+
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this method to break!
-// console.log(joe.greeting());
+console.log(joe.greeting());
 
 // TODO: After viewing the previous console.log(), return the code to a working state.
 
 
 
-Student.courseName = function() {
-  return 'This student is enrolled in Code 301.';
-};
+// Student.courseName = function() {
+//   return 'This student is enrolled in Code 301.';
+// };
+
+Student.courseName = ()  => 'This student is enrolled in Code 301.'
+
+
+
+
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(Student.courseName());
+console.log(Student.courseName());
 
 
 
@@ -190,17 +230,20 @@ Student.prototype.scope = function() {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scope());
+console.log(joe.scope());
 
 Student.prototype.scopeArrow = () => console.log(this);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scopeArrow());
+console.log(joe.scopeArrow());
 
 // TODO: Write a COMMENT below to answer the following questions.
-// 1. What is "this" when joe.scope() is invoked?
+// 1. What is "this" when joe.scope() is invoked?   https://www.youtube.com/watch?v=GhbhD1HR5vk&t=116s
+//undefined - due to 'this' is not a global varable
 //
 // 2. What is "this" when joe.scopeArrow() is invoked?
+// it's calling the window object instead of 'this'
 //
 // 3. Explain why "this" is different when an arrow function is used.
 //
+//  The arrow function doesn't BIND the object 'this'.  
